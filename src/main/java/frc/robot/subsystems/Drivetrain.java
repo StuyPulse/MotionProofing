b8585d84691ff55eb0ac4fdb330ac897730662d3
prog694 <prog694@gmail.com>
2019-10-15 17:59:29 -0400

    Add gyro and encoder methods, remove pathfollowing

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -7,58 +7,43 @@
 
 package frc.robot.subsystems;
 
-import java.io.File;
-import java.io.IOException;
-
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import jaci.pathfinder.Pathfinder;
-import jaci.pathfinder.Trajectory;
-import jaci.pathfinder.followers.EncoderFollower;
 import frc.robot.RobotMap;
 
 public class Drivetrain extends Subsystem {
 
+  private WPI_TalonSRX leftFrontMotor, leftRearMotor, rightFrontMotor, rightRearMotor;
+
   private Encoder leftEncoder, rightEncoder;
 
-  private WPI_TalonSRX leftFrontMotor, leftRearMotor, rightFrontMotor, rightRearMotor;
+  private AHRS gyro;
 
   private SpeedControllerGroup leftMotors, rightMotors;
 
   private DifferentialDrive differentialDrive;
 
-  private Trajectory leftTrajectory, rightTrajectory;
+  public Drivetrain() {
+    leftRearMotor = new WPI_TalonSRX(RobotMap.PORTS.TEMP_PORT);
+    leftFrontMotor = new WPI_TalonSRX(RobotMap.PORTS.TEMP_PORT);
+    rightRearMotor = new WPI_TalonSRX(RobotMap.PORTS.TEMP_PORT);
+    rightFrontMotor = new WPI_TalonSRX(RobotMap.PORTS.TEMP_PORT);
+    
+    leftEncoder = new Encoder(RobotMap.PORTS.TEMP_PORT, RobotMap.PORTS.TEMP_PORT);
+    rightEncoder = new Encoder(RobotMap.PORTS.TEMP_PORT, RobotMap.PORTS.TEMP_PORT);
 
-  private EncoderFollower left, right;
-
-  public Drivetrain() throws IOException{
-    leftEncoder = new Encoder(RobotMap.TEMP_PORT, RobotMap.TEMP_PORT);
-    rightEncoder = new Encoder(RobotMap.TEMP_PORT, RobotMap.TEMP_PORT);
-
-    leftRearMotor = new WPI_TalonSRX(RobotMap.TEMP_PORT);
-    leftFrontMotor = new WPI_TalonSRX(RobotMap.TEMP_PORT);
-    rightRearMotor = new WPI_TalonSRX(RobotMap.TEMP_PORT);
-    rightFrontMotor = new WPI_TalonSRX(RobotMap.TEMP_PORT);
+    gyro = new AHRS(I2C.Port.kMXP);
 
     leftMotors = new SpeedControllerGroup(leftRearMotor, leftFrontMotor);
     rightMotors = new SpeedControllerGroup(rightRearMotor, rightFrontMotor);
 
     differentialDrive = new DifferentialDrive(leftMotors, rightMotors);
-
-    leftTrajectory = Pathfinder.readFromCSV(new File("../CSV/Trajectory_left.csv"));
-    rightTrajectory = Pathfinder.readFromCSV(new File("../CSV/Trajectory_right.csv"));
-
-    left = new EncoderFollower(leftTrajectory);
-    right = new EncoderFollower(rightTrajectory);
-
-    left.configurePIDVA(RobotMap.KP, RobotMap.KI, RobotMap.KD, 1/RobotMap.MAX_VEL, RobotMap.MAX_ACCEL);
-    right.configurePIDVA(RobotMap.KP, RobotMap.KI, RobotMap.KD, 1/RobotMap.MAX_VEL, RobotMap.MAX_ACCEL);
-    left.configureEncoder(0, RobotMap.ENCODER_TPR, RobotMap.WHEEL_DIAMETER);
-    right.configureEncoder(0, RobotMap.ENCODER_TPR, RobotMap.WHEEL_DIAMETER);
   }
 
   @Override
@@ -71,7 +56,19 @@ public class Drivetrain extends Subsystem {
     differentialDrive.tankDrive(leftSpeed, rightSpeed);
   }
 
-  public void autoDrive(){
-    tankDrive(left.calculate(leftEncoder.get()), right.calculate(rightEncoder.get()));
+  public int getLeftEncoderTicks() {
+    return leftEncoder.get();
+  }
+
+  public int getRightEncoderTicks() {
+    return rightEncoder.get();
+  }
+
+  public void stop(){
+    tankDrive(0, 0);
+  }
+
+  public double getGyroAngle(){
+    return gyro.getAngle();
   }
 }
