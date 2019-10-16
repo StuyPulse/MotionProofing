b6bf184403f7e323db1ad1dba35dba77c4af5d63
prog694 <prog694@gmail.com>
2019-10-16 17:51:16 -0400

    Add DEStiny ports, Add some dimensions, Edit constants to make more accurate

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -9,21 +9,33 @@ package frc.robot;
 
 public interface RobotMap {
     public interface PORTS{
-        int TEMP_PORT = -1;
+        int FRONT_RIGHT_MOTOR_CHANNEL = 1;
+        int REAR_RIGHT_MOTOR_CHANNEL = 2;
+        int REAR_LEFT_MOTOR_CHANNEL = 3;
+        int FRONT_LEFT_MOTOR_CHANNEL = 4;
+
+        int LEFT_ENCODER_CHANNEL_A = 0;
+        int LEFT_ENCODER_CHANNEL_B = 1;
+        int RIGHT_ENCODER_CHANNEL_A = 2;
+        int RIGHT_ENCODER_CHANNEL_B = 3;
     }
     
     public interface MATH_CONSTANTS{
-        double MAX_VEL = 50;
+        double MAX_VEL = 1.7;
         double MAX_ACCEL = 0.2;
     }
 
     public interface HARDWARE_DETAIL{
-        int ENCODER_TICKS_PER_REV = 100;
-        double WHEEL_DIAMETER = 10.0;
+        int ENCODER_TICKS_PER_REV = 256;
+
+        // In feet to match field maps
+        double CHASSIS_WIDTH = 20.875/12;
+        double WHEEL_TO_WHEEL_WIDTH = 25.5/12;
+        double WHEEL_DIAMETER = 7.5;
     }
 }
-/*
+/*.
     Ask if encoder is quad, and ask for ticks per rate
-    Ask for max vel and max accel
+    Test maxvel
     Ask Kevin if it goes clockwise or countercl
 */
