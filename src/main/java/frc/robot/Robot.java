3fd395fef499f4570883a9a497d7000f43d47681
prog694 <prog694@gmail.com>
2019-10-16 17:46:38 -0400

    Add DrivetrainFollowPathCommand to AutonChooser

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,8 +8,10 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.commands.DrivetrainFollowPathCommand;
 import frc.robot.subsystems.Drivetrain;
 
 /**
@@ -20,10 +22,8 @@ import frc.robot.subsystems.Drivetrain;
  * project.
  */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  private final SendableChooser<Command> m_chooser = new SendableChooser<>();
 
   public static Drivetrain drivetrain;
 
@@ -35,8 +35,7 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     drivetrain = new Drivetrain();
     
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
+    m_chooser.setDefaultOption("Default Auto", new DrivetrainFollowPathCommand("paths/Trajectory_left.csv", "paths/Trajectory_right.csv"));
     SmartDashboard.putData("Auto choices", m_chooser);
 
     SmartDashboard.putNumber("Kp", 0);
@@ -69,9 +68,10 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    m_autoSelected = m_chooser.getName();
     System.out.println("Auto selected: " + m_autoSelected);
+
+    m_chooser.getSelected().start();
   }
 
   /**
@@ -79,15 +79,6 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
   }
 
   /**
